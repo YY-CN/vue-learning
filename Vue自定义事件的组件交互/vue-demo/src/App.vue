48d07cb11099da0521d345924548201cabb58dbe
YY <YY20201122@126.com>
2023-05-10 23:53:12 +0800

    修改名称

---
+++ Vue自定义事件的组件交互/vue-demo/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{ testmessage }}</h1>
+<!-- 利用上个组件的自定义事件来接收数据 -->
+<!-- 上个组件定义了自定义事件，在父组件里面触发这个事件@onEvent来获取传递过来的值 -->
+  <my-component @onEvent = "getDataHandle" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent
+  },
+  data(){
+    return{
+      testmessage:"还未传递过来信息点击传递"
+    }
+  },
+  methods:{
+    // 触发事件后调用该方法来获取子组件传递过来的值
+    // 传递回来的数据在参数里面data
+    getDataHandle(data){
+      console.log(data);
+      this.testmessage = data;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue自定义事件的组件交互/vue-demo/src/components/MyComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <h3>
+        自定义传递数据
+    </h3>
+   <button @click="sendClick">点击传递</button>
+    
+    
+    </template>
+    
+    <script>
+        export default{
+
+            // 本组件名称
+            name:"MyComponent",
+
+            // 为父组件传递过来的值
+            props:{
+                
+                
+            },
+
+
+            // data中为页面中要使用(显示)的数据
+            data(){
+                return{
+                    message:"我是点击事件的MyComponent传递"
+                }
+            },
+
+            // 本组件要是用的方法
+            methods:{
+                sendClick(){
+                    // 参数1 “onEvent”：字符串，为传递的数据的自定义事件名（需要具有意义）onEvent是一个事件
+                    // 参数2 “传递的数据”   即需要传递过去的数据作为键值
+                    this.$emit("onEvent",this.message);
+                }
+            }
+        }
+    
+    </script>
+    <style scoped>
+    h3{
+        color: aquamarine;
+    }
+    </style>
